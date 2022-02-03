f323c0c38c1d0f6ea9873b5250b265ff1fe99232
Suresh Reddy <sureshreddy@engineer.com>
2022-02-03 23:37:42 +0530

    correct grammar mistakes

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,7 +20,7 @@ const faqs = [
     qid: 3,
     title: "Who are the targetted audience of this site?",
     description:
-      "Fellow Bengalurians and people living/travelling to Karnataka as most of the Food Lovers TV Vlogs are from this region",
+      "Fellow Bengalurians and people living/travelling around Karnataka as most of the Food Lovers TV Vlogs are from this region",
   },
   {
     qid: 4,
@@ -33,7 +33,7 @@ const faqs = [
     qid: 5,
     title: "Is FLDb owned or sponsored by Kripal Amanna/Food Lovers TV",
     description:
-      "Nope. Although, the name is inspired from his youtube channel: Food Lovers TV",
+      "Nope. Although, the name is inspired from the youtube channel",
   },
   {
     qid: 6,
@@ -45,25 +45,25 @@ const faqs = [
     qid: 7,
     title: "How does FLDb plan to make money?",
     description:
-      "The plan is to not make money. You heard me right, this site is free to all and does not include any ads, paid promotions or sponsorship. This site is a resultant of what started as my hobby project",
+      "The plan is to not make money. You heard me right, this site is free to all and does not include any ads, paid promotions or sponsorship. This site is a result of what started as my hobby project",
   },
   {
     qid: 8,
     title: "How will FLDb sustain without making any money?",
     description:
-      "This site depends on a bunch of free services offered by companies like Heroku.com, Netlify.com, GitHub.com and MongoDB.com. The only things that can't be obtained for free is the domain ownership and a bunch of API calls made to Google Places API which I is ready to bear myself. I am commited to keep this site free for ever given that it doesn't punch a hole in my pocket",
+      "This site depends on a bunch of free services offered by companies like Heroku.com, Netlify.com, GitHub.com and MongoDB.com. The only things that can't be obtained for free is the domain ownership and a bunch of API calls made to Google Places API which I am ready to bear myself. I will continue to keep this site free from ads, promotions, sponsorships etc., as long as it doesn't punch a hole in my pocket",
   },
   {
     qid: 9,
     title: "How can I contribute to FLDb?",
     description:
-      "While financial contributions is not encouraged, you can always contribute from technical point of view by submitting feature requests, bug report, code contribution etc., @ https://github.com/Mr-SKR/FLDB/issues or marketing by spreading the existence of this site by word of mouth. I'd genuinely feel happy if somebody recommends this site to me someday by the power of word of mouth",
+      "While financial contributions are not encouraged, you can always contribute from technical point of view by submitting feature requests, bug report, code contribution etc., @ https://github.com/Mr-SKR/FLDB/issues or marketing by spreading the existence of this site by word of mouth. I'd genuinely feel happy if somebody recommends this site to me someday through the power of the word of mouth",
   },
   {
     qid: 10,
     title: "Is this site highly available or scalable?",
     description:
-      "Nope. This site depends on a bunch of free services offered by companies like Heroku.com, Netlify.com, GitHub.com and MongoDB.com. These free services can't guarantee high availability and scalability. Since I do not expect a high amount of traffic flowing to this site, this site should manage just fine",
+      "Nope. This site depends on a bunch of free services offered by companies like Heroku.com, Netlify.com, GitHub.com and MongoDB.com. These free services can't guarantee high availability and scalability. Since I do not expect a high amount of traffic flowing to this site, this site should manage just fine. My load testing results say that 30 concurrent users can use this site without facing any issues",
   },
   {
     qid: 11,
