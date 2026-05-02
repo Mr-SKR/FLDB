317e865e0640989ddf04e1cc0f8087af71d2330c
Suresh Reddy <sureshreddy@engineer.com>
2026-05-03 00:31:02 +0530

    fix casing

---
+++ components/accordion/Accordion.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Grid,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
+import { ExpandMore as ExpandMoreIcon } from "@mui/icons-material";
+import Linkify from "linkify-react";
+
+interface CustomAccordionProps {
+  qid: number;
+  title: string;
+  description: string;
+}
+
+export default function CustomAccordion(
+  props: CustomAccordionProps
+): React.ReactElement {
+  return (
+    <Grid item xs={12}>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          id={`${props.qid}-accordion`}
+        >
+          <Typography>{props.title}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Linkify tagName="p">
+            {props.description ? String(props.description) : "N/A"}
+          </Linkify>
+        </AccordionDetails>
+      </Accordion>
+    </Grid>
+  );
+}
